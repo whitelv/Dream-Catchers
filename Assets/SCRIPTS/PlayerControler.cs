7eb5c4fb89be4dc8babf2ab9b21446b1843e14ad
Любомир Витвицький <lubomyr0901@gmail.com>
2024-01-23 14:49:51 +0200

    movement and colission

---
+++ Assets/SCRIPTS/PlayerControler.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private bool isMoving;
+
+    private Vector2 input;
+
+    //private Animator animator;
+
+    public LayerMask solidObjectLayer;
+
+    //private void Awake()
+    //{
+    //    animator = GetComponent<Animator>();
+    //}
+
+    private void Update()
+    {
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            Debug.Log("This is input.x" + input.x);
+            Debug.Log("This is input.y" + input.y);
+            if (input.x != 0) input.y = 0;
+
+            if (input != Vector2.zero)
+            {
+
+                //animator.SetFloat("moveX", input.x);
+                //animator.SetFloat("moveY", input.y);
+
+                var targetPos = transform.position;
+                targetPos.x += input.x / 10;
+                targetPos.y += input.y / 10;
+
+                if (isWalkable(targetPos))
+
+                    StartCoroutine(Move(targetPos));
+            }
+        }
+
+        //animator.SetBool("isMoving", isMoving);
+
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+
+    private bool isWalkable(Vector3 targetPos)
+    {
+        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectLayer) != null)
+        {
+            return false;
+        }
+        return true;
+
+    }
+
+}
